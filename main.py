5583e3fd79c6012d12dd0e17627c1fd53ec59ee7
Mirgahney <mmerghaney@aimsammi.org>
2019-07-07 05:59:56 +0200

    remove test cases

--- main.py
+++ main.py
@@ -153,6 +153,7 @@ result_df = pd.DataFrame(columns=['step', 'mll'])#, 'accuracy'])
 
 # progress bar information
 tdqm = conv_utils.TqdmExtraFormat
+
 mll_max = -np.inf
 accuracy_list = []
 mll_list = [mll_max, mll_max, mll_max]
@@ -167,7 +168,7 @@ for i in tdqm(
     if i % 500 == 1:
         print("Iteration {}".format(i))
         mll = model.print_sample_performance()
-        if i >= 500:
+        if i >= 17500:
             if np.round(mll - mll_max, decimals = 5) > 0:
                 # accuracy = measure_accuracy(model)
                 mll_max = mll
@@ -208,10 +209,10 @@ def save_result(result_df, save_dir, name = None):
 
 model.save(flags.out)
 
-accuracy = 0#measure_accuracy(model)
+accuracy = measure_accuracy(model)
 # loop over model
 for m in tdqm(best_model_que):
-    acc = 0#measure_accuracy(m)
+    acc = measure_accuracy(m)
     accuracy_list.append(acc)
 
 acc_ind = np.argmax(accuracy_list)
