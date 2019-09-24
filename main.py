5e49fe8083a8ceb38194e32a175fc1f637cc2ba7
Mirgahney Mohamed <mmerghaney@aimsammi.org>
2019-09-24 21:53:07 +0200

    add ResCGPNet class

--- main.py
+++ main.py
@@ -141,6 +141,37 @@ def Basic_Block(input_size, inplanes = None, planes = None, stride = 1, downsamp
 
     return layers, input_size
 
+class ResCGPNet():
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+
+        if layer == 0:
+            Z = patches
+        else:
+            Z = Z_inner
+        
+        filter_size = filters[layer]
+
+
 if flags.load is not None:
     print("Loading parameters")
     checkpoint = tf.train.latest_checkpoint(flags.load)
